c85e2bc2f26f573d06c516669cabd924f196fa68
Bob Clapp <bob@sep.stanford.edu>
2023-05-20 14:53:37 -0700

    start name conversion

---
+++ generic_solver/__init__.py
@@ -0,0 +1,5 @@
+from generic_solver._pyVector import vector as Vector
+from generic_solver._pyVector import vectorIC as VectorIC
+from generic_solver._pyVector import vectorSet as VectorSet
+from generic_solver._pyVector import vectorOC as VectorOC
+from generic_solver._pyVector import superVector as superVector
